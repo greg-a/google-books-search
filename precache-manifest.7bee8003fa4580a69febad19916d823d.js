9494076a11fa9c54071213b83e10420596684cde
Greg Allebach <greg.allebach@gmail.com>
2020-08-23 09:13:33 -0400

    Updates

---
+++ precache-manifest.7bee8003fa4580a69febad19916d823d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b32f2d321971d3934085ab3c3b8360",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "1b7427c7b76de3d0e5ab",
+    "url": "/google-books-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83b1528953aebe8b5d62",
+    "url": "/google-books-search/static/js/2.100a69cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-books-search/static/js/2.100a69cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7427c7b76de3d0e5ab",
+    "url": "/google-books-search/static/js/main.918435e6.chunk.js"
+  },
+  {
+    "revision": "8465395b8b0447a9cccd",
+    "url": "/google-books-search/static/js/runtime-main.8b62a3e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
