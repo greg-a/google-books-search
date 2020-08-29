799914d0982c9ad10acc10a2972896c23bd37fa9
Greg Allebach <greg.allebach@gmail.com>
2020-08-29 10:18:31 -0400

    added line in server.js to troubleshoot

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password123@ds151076.mlab.com:51076/heroku_dg4r5fl2");
 
